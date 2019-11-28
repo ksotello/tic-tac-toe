0ec6cd21705c50c1b8250e414a388f73676a2e2f
Kyle Sotello <opticalvoid@gmail.com>
2019-11-27 21:20:56 -0500

    Generating 9 Spaces

--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -1,3 +1,21 @@
 import React from 'react';
 
-export default () => <div />;
+import { Space } from "."
+
+const generateSpaces = () => {
+    let spaces = [];
+
+    for (let i = 0; i < 9; i++) {
+        spaces.push(Space);
+    }
+
+    return spaces;
+}
+
+export default () => (
+    <div className="--wrapper">
+        {generateSpaces().map((space, index) => {
+            return React.createElement(space, { index });
+        })}
+    </div>
+)
