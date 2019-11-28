1873564fe1608f0244753f196d07e24e46aa2088
Kyle Sotello <opticalvoid@gmail.com>
2019-11-27 21:20:27 -0500

    Adding test: should display a grid with 9 spaces

--- src/__tests__/GameGridSpec.js
+++ src/__tests__/GameGridSpec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import { GameGrid } from '../components';
+import { GameGrid, Space } from '../components';
 
 describe('<GameGrid />', () => {
     let wrapper;
@@ -14,6 +14,12 @@ describe('<GameGrid />', () => {
     it('should mount without issue', done => {
         wrapper = mount(<GameGrid />)
         expect(wrapper.exists()).toBe(true);
+        done();
+    });
+
+    it('should display a grid with 9 spaces', done => {
+        wrapper = mount(<GameGrid />);
+        expect(wrapper.find(Space).length).toEqual(9)
         done();
     });
 
