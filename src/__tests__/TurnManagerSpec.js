59ee8b2ce74cba0ce52730019e19ba99eaaf23f2
Kyle Sotello <opticalvoid@gmail.com>
2019-11-27 21:57:22 -0500

    adding check to make sure that the names of player1 and player2 get set when the turn manager initializes

--- src/__tests__/TurnManagerSpec.js
+++ src/__tests__/TurnManagerSpec.js
@@ -4,4 +4,10 @@ describe('The TurnManager', () => {
     it('should be defined', () => {
         expect(TurnManager).toBeDefined();
     });
+
+    it('should set player1 and player2 when initialized ', () => {
+        const turnManager = new TurnManager({ player1: "bruce", player2: "sue" });
+        expect(turnManager.player1).toEqual("bruce");
+        expect(turnManager.player2).toEqual("sue");
+    });
 });
