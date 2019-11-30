3a8dd540f06c2a7f24f22ea72ae6b4772dcba465
Kyle Sotello <opticalvoid@gmail.com>
2019-11-30 14:17:31 -0500

    adding currentTurn state, getCurrentTurn and advanceTurn functionality in context

--- src/components/GameManager.js
+++ src/components/GameManager.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TurnManager } from '../classes';
 import { GameContext } from '../providers';
 
 const GameManager = ({ player1, player2, children }) => {
     const turnManager = new TurnManager({ player1, player2 });
+    const [currentTurn, setCurrentTurn] = useState(0);
+
+    const getCurrentTurn = () => currentTurn;
+    const advanceTurn = () => setCurrentTurn(currentTurn + 1);
 
     return (
-        <GameContext.Provider value={turnManager}>
+        <GameContext.Provider value={{ ...turnManager, getCurrentTurn, advanceTurn }}>
             {children}
         </GameContext.Provider>
     );
