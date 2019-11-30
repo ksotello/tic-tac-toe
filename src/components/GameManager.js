2d88b53e9238b0ba114d4a16a3b8af6048fbe517
Kyle Sotello <opticalvoid@gmail.com>
2019-11-30 15:14:30 -0500

    add code to advance turn

--- src/components/GameManager.js
+++ src/components/GameManager.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TurnManager } from '../classes';
 import { GameContext } from '../providers';
 
 const GameManager = ({ player1, player2, children }) => {
     const turnManager = new TurnManager({ player1, player2 });
-    const [currentTurn, setCurrentTurn] = useState(0);
+    let currentTurn = 0;
 
     const getCurrentTurn = () => currentTurn;
-    const advanceTurn = () => setCurrentTurn(currentTurn + 1);
+    const advanceTurn = () => ++currentTurn;
 
     return (
         <GameContext.Provider value={{ ...turnManager, getCurrentTurn, advanceTurn }}>
