557f9b2a148c9e7e6529e0a02cf659ee8a0cf07a
Kyle Sotello <opticalvoid@gmail.com>
2019-11-30 11:54:40 -0500

    adding test to pass turn information via context to the game managers children

--- src/__tests__/GameManagerSpec.js
+++ src/__tests__/GameManagerSpec.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { mount } from 'enzyme';
 
 import { GameManager } from '../components';
+import { GameContext } from '../providers';
 
 describe('<GameManager />', () => {
     let wrapper;
@@ -14,6 +15,26 @@ describe('<GameManager />', () => {
     it('should mount without issue', done => {
         wrapper = mount(<GameManager />)
         expect(wrapper.exists()).toBe(true);
+        done();
+    });
+
+    it('should provide turn information', done => {
+        const GameManagerChild = () => {
+            const gameContext = useContext(GameContext);
+
+            return <div {...gameContext}></div>
+        };
+
+        const wrapper = mount(
+            <GameManager player1="bruce" player2="sue">
+                <GameManagerChild />
+            </GameManager>
+        );
+
+        const props = wrapper.find("div").props();
+        expect(props.player1).toEqual("bruce");
+        expect(props.player2).toEqual("sue");
+
         done();
     });
 });
