bdec03702b161e27e24ff6f64404448e049c0139
Kyle Sotello <opticalvoid@gmail.com>
2019-11-30 10:33:40 -0500

    adding check for player turns

--- src/__tests__/TurnManagerSpec.js
+++ src/__tests__/TurnManagerSpec.js
@@ -1,13 +1,22 @@
 import { TurnManager } from '../classes';
 
 describe('The TurnManager', () => {
+    const player1 = "bruce";
+    const player2 = "sue";
+
     it('should be defined', () => {
         expect(TurnManager).toBeDefined();
     });
 
     it('should set player1 and player2 when initialized ', () => {
-        const turnManager = new TurnManager({ player1: "bruce", player2: "sue" });
+        const turnManager = new TurnManager({ player1, player2 });
         expect(turnManager.player1).toEqual("bruce");
         expect(turnManager.player2).toEqual("sue");
     });
+
+    it('should keep track of turns', () => {
+        const turnManager = new TurnManager({ player1, player2 });
+        expect(turnManager.turnHistory[player1].turns instanceof Array).toBe(true);
+        expect(turnManager.turnHistory[player2].turns instanceof Array).toBe(true);
+    })
 });
