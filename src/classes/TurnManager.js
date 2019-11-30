0e742a1285d7a140624efa78cc1301dc26cfd6f9
Kyle Sotello <opticalvoid@gmail.com>
2019-11-30 10:34:09 -0500

    adding turns for players 1 and 2

--- src/classes/TurnManager.js
+++ src/classes/TurnManager.js
@@ -3,6 +3,15 @@ class TurnManager {
     constructor({ player1, player2 }) {
         this.player1 = player1;
         this.player2 = player2;
+
+        this.turnHistory = {
+            [player1]: {
+                turns: []
+            },
+            [player2]: {
+                turns: []
+            }
+        }
     }
 }
 
