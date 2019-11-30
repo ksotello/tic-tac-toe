cc2056dcf503d648432ce996d128b937e2259a88
Kyle Sotello <opticalvoid@gmail.com>
2019-11-30 11:55:24 -0500

    adding turn manager instance to game manager context

--- src/components/GameManager.js
+++ src/components/GameManager.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { TurnManager } from '../classes';
+import { GameContext } from '../providers';
 
-const GameManager = () => <div />;
+const GameManager = ({ player1, player2, children }) => {
+    const turnManager = new TurnManager({ player1, player2 });
+
+    return (
+        <GameContext.Provider value={turnManager}>
+            {children}
+        </GameContext.Provider>
+    );
+};
 
 export default GameManager;
