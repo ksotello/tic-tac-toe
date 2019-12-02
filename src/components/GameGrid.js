568435e1ee98ce57e7a5142767e0aafc6e5bb168
Kyle Sotello <opticalvoid@gmail.com>
2019-12-01 23:35:24 -0500

    adding game grid to main app; adding x y coords to space

--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -13,10 +13,21 @@ const generateSpaces = () => {
     return spaces;
 }
 
+const getY = index => {
+    if (index <= 2) {
+        return 0;
+    } else if (index > 2 && index <= 5) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
 export default () => (
     <Grid container className="--wrapper">
         {generateSpaces().map((space, index) => {
-            return React.createElement(space, { index });
+            console.log(index);
+            return React.createElement(space, { x: index, y: getY(index), key: index, value: "X" });
         })}
     </Grid>
 )
--- src/components/Space.js
+++ src/components/Space.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-const Space = ({ value }) => <Grid item>{value}</Grid>
+const Space = ({ value, x, y, key }) => <Grid item key={key} x={x} y={y} xs={4}>{value}</Grid>
 
 export default Space;
